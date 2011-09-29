60b74965e79c53d7ac4690fd347bb4daa6adcaba
william0wang <william@mplayer-ww.com>
2011-09-29 17:18:43 +0800

    Add auto merge script

---
+++ automerge.sh
@@ -0,0 +1,12 @@
+# !/bin/sh
+tmpFile="autom.sh"
+
+echo "git checkout official" >> $tmpFile
+echo "git pull official master" >> $tmpFile
+echo "git checkout console" >> $tmpFile
+echo "git merge official" >> $tmpFile
+echo "git checkout master" >> $tmpFile
+echo "git merge official" >> $tmpFile
+echo "rm -f "$tmpFile >> $tmpFile
+chmod 777 $tmpFile
+exec ./$tmpFile
