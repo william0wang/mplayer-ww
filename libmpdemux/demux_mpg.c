3bba604ad05362be24161775be5d0e93ebc7a1cb
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2013-12-11 20:03:02 +0000

    Fix potential endless loop while probing.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36542 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libmpdemux/demux_mpg.c
+++ libmpdemux/demux_mpg.c
@@ -84,6 +84,8 @@ static int parse_psm(demuxer_t *demux, int len) {
   es_map_len = stream_read_word(demux->stream);		//length of elementary streams map
   es_map_len = FFMIN(es_map_len, len - prog_len - 8);	//sanity check
   while(es_map_len > 0) {
+    if (demux->stream->eof)
+      return 0;
     type = stream_read_char(demux->stream);
     id = stream_read_char(demux->stream);
     if(id >= 0xB0 && id <= 0xEF && priv) {
