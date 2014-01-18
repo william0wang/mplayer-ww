54dde722a07361328b1b68185deda01ca5e52a09
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2014-01-18 23:08:33 +0000

    vo_sdl: Avoid code duplication in DR code.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36618 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_sdl.c
+++ libvo/vo_sdl.c
@@ -1327,16 +1327,14 @@ static uint32_t get_image(mp_image_t *mpi)
         if(mpi->flags&MP_IMGFLAG_PLANAR) {
 	    mpi->planes[0] = priv->overlay->pixels[0] + priv->y*priv->overlay->pitches[0];
 	    mpi->stride[0] = priv->overlay->pitches[0];
-	    if(mpi->flags&MP_IMGFLAG_SWAPPED) {
-		mpi->planes[1] = priv->overlay->pixels[1] + priv->y*priv->overlay->pitches[1]/2;
-		mpi->stride[1] = priv->overlay->pitches[1];
-		mpi->planes[2] = priv->overlay->pixels[2] + priv->y*priv->overlay->pitches[2]/2;
-		mpi->stride[2] = priv->overlay->pitches[2];
-	    } else {
-		mpi->planes[2] = priv->overlay->pixels[1] + priv->y*priv->overlay->pitches[1]/2;
-		mpi->stride[2] = priv->overlay->pitches[1];
-		mpi->planes[1] = priv->overlay->pixels[2] + priv->y*priv->overlay->pitches[2]/2;
-		mpi->stride[1] = priv->overlay->pitches[2];
+	    mpi->planes[1] = priv->overlay->pixels[1] + priv->y*priv->overlay->pitches[1]/2;
+	    mpi->stride[1] = priv->overlay->pitches[1];
+	    mpi->planes[2] = priv->overlay->pixels[2] + priv->y*priv->overlay->pitches[2]/2;
+	    mpi->stride[2] = priv->overlay->pitches[2];
+	    // SDL order is considered swapped by MPlayer
+	    if(!(mpi->flags&MP_IMGFLAG_SWAPPED)) {
+		    FFSWAP(void *, mpi->planes[1], mpi->planes[2]);
+		    FFSWAP(int, mpi->stride[1], mpi->stride[2]);
 	    }
         }
         else if(IMGFMT_IS_RGB(priv->format) || IMGFMT_IS_BGR(priv->format)) {
