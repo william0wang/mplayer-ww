b83837bf02b308b322951c95a816a7b04b42fb8b
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2014-01-23 17:28:02 +0000

    Cosmetic: Adjust spacings.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36710 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/dialog/fileselect.c
+++ gui/dialog/fileselect.c
@@ -123,15 +123,15 @@ int fsLastAudioFilterSelected = -1;
 
 char *fsFontFileNames[][2] = {
 #ifdef CONFIG_FREETYPE
-    { MSGTR_Filter_TTF,       "*.ttf"       },
-    { MSGTR_Filter_Type1,     "*.pfb"       },
+    { MSGTR_Filter_TTF,       "*.ttf"              },
+    { MSGTR_Filter_Type1,     "*.pfb"              },
 #endif
-    { MSGTR_Filter_FontFiles, "*.desc"      },
+    { MSGTR_Filter_FontFiles, "*.desc"             },
 #ifdef CONFIG_FREETYPE
     { MSGTR_Filter_AllFonts,  "*.pfb,*.ttf,*.desc" },
 #endif
-    { MSGTR_Filter_AllFiles,  "*"           },
-    { NULL,                   NULL          }
+    { MSGTR_Filter_AllFiles,  "*"                  },
+    { NULL,                   NULL                 }
 };
 int fsLastFontFilterSelected = -1;
 
