aaebfb2684ac4eb1ed3a0042aa3ce85b07f45113
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2014-02-24 12:23:14 +0000

    Adjust range and step increment of subfps to fps.
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36924 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/dialog/preferences.c
+++ gui/dialog/preferences.c
@@ -805,7 +805,7 @@ static GtkWidget * CreatePreferences( void )
   HSSubPosition=gtkAddHScale( HSSubPositionadj,NULL,0 );
     gtk_table_attach( GTK_TABLE( table1 ),HSSubPosition,1,2,1,2,(GtkAttachOptions)( GTK_EXPAND | GTK_FILL ),(GtkAttachOptions)( 0 ),0,0 );
 
-  HSSubFPSadj=GTK_ADJUSTMENT( gtk_adjustment_new( 0,0,100,0.01,0,0 ) );
+  HSSubFPSadj=GTK_ADJUSTMENT( gtk_adjustment_new( 0,0,120,0.001,0,0 ) );
   HSSubFPS=gtk_spin_button_new( GTK_ADJUSTMENT( HSSubFPSadj ),1,3 );
     gtk_widget_show( HSSubFPS );
     gtk_widget_set_usize( HSSubFPS,60,-1 );
