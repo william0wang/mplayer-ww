2d30f1d82e6f223aed8a22975adfac14f5fbf5f4
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2014-03-02 21:06:26 +0000

    Remove VOCTRL_GUISUPPORT.
    The video output driver doesn't support it.
    
    
    git-svn-id: svn://git.mplayerhq.hu/mplayer/trunk@36967 b3059339-0415-0410-9bf9-f77b7e298cf2

--- libvo/vo_dfbmga.c
+++ libvo/vo_dfbmga.c
@@ -1408,9 +1408,6 @@ static int
 control( uint32_t request, void *data )
 {
      switch (request) {
-     case VOCTRL_GUISUPPORT:
-          return VO_TRUE;
-
      case VOCTRL_QUERY_FORMAT:
           return query_format( *((uint32_t *) data) );
 
