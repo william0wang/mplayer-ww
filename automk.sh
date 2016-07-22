c45041e9402cb06671582510e9b3332ddac21a39
William <william@antutu.com>
2016-07-22 14:39:42 +0800

    Fixed info

--- automk.sh
+++ automk.sh
@@ -4,12 +4,12 @@ test "$1" && _tg="$1"
 
 if test $_tg = st || test $_tg = shared; then
   echo
-  echo Beginning build static version...
+  echo Beginning build sharded version...
   echo
   ./automk-shared.sh $2 $3 $4
 else
   echo
-  echo Beginning build sharded version...
+  echo Beginning build static version...
   echo
   ./automk-static.sh $2 $3 $4
 fi
